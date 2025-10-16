0e17b8ce77bc76af971c10d788a22ee25baf231a
Yusif Alaidy <yusifalaidy@gmail.com>
2025-10-16 18:02:12 +0300

    feat: create swagger Documentation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace TagerCom
 {
@@ -8,24 +12,42 @@ namespace TagerCom
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+            // ? Optional: Keep OpenAPI (generates JSON)
             builder.Services.AddOpenApi();
+
+            // ? Add Swagger UI support
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "TagerCom API",
+                    Version = "v1",
+                    Description = "API documentation for TagerCom project"
+                });
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // ? Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                // This generates openapi.json
                 app.MapOpenApi();
+
+                // ? Enable Swagger UI
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "TagerCom API v1");
+                    options.RoutePrefix = "swagger"; // open at /swagger
+                });
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
--- obj/Debug/net9.0/TagerCom.AssemblyInfo.cs
+++ obj/Debug/net9.0/TagerCom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TagerCom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a7e9d49eb69fe328fafadaa9feae0b023d38ea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17cf9c69677c1200ec3e4428e06e4f865f8cb684")]
 [assembly: System.Reflection.AssemblyProductAttribute("TagerCom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TagerCom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
