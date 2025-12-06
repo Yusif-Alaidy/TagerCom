befad38f8bcb68cd68011293c4546c08308ed737
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2025-12-06 14:36:44 +0200

    feat(reviews): add paginated endpoint to fetch product reviews (newest first)

--- Areas/Customer/DTOs/Request/CreateReview.cs
+++ Areas/Customer/DTOs/Request/CreateReview.cs
@@ -6,10 +6,10 @@ namespace TagerCom.Areas.Customer.DTOs.Request
     {
         public Guid Productid { get; set; }
 
-        [Range(1,5)]
-        public int Review { get; set; }
+        [MaxLength(1000, ErrorMessage="Comment is too long , please make it shorter"),]
         public string Comment { get; set; }
 
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
         public int Rating { get; set; }
 
     }
--- Areas/Customer/DTOs/Request/ProductRatingDTO.cs
+++ Areas/Customer/DTOs/Request/ProductRatingDTO.cs
@@ -6,6 +6,8 @@
         public Guid ProductId { get; set; }
         public double AverageRating { get; set; }
         public int ReviewsCount { get; set; }
+        public List<string?> Comments { get; set; } = new();
+
 
     }
 }
