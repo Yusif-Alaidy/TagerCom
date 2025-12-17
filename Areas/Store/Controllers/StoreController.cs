4d1014dd8005bf97c96ba07ab42f0c460adeca8e
Yusif_Alaidy <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-12-17 19:20:02 +0200

    feat: build view profile action

---
+++ Areas/Store/Controllers/StoreController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using TagerCom.Areas.Store.DTOs.Response;
+
+namespace TagerCom.Areas.Store.Controllers
+{
+    [Area("Store")]
+    [Route("api/store/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Vendor")]
+    public class StoreController : ControllerBase
+    {
+        #region Fields
+        public UserManager<ApplicationUser> UserManager { get; }
+        public IRepository<Models.Store> StoreRepo { get; }
+        #endregion
+
+        #region Constructore
+        public StoreController(UserManager<ApplicationUser> UserManager, IRepository<Models.Store> StoreRepo)
+        {
+            this.UserManager = UserManager;
+            this.StoreRepo = StoreRepo;
+        }
+
+        #endregion
+
+        #region view store
+        [HttpGet]
+        public async Task<IActionResult> GetStore()
+        {
+            // Get user ================================================
+            var user = await UserManager.GetUserAsync(User);
+            // =========================================================
+
+            // Get Store ===============================================
+            var store = (await StoreRepo.GetOneAsync(e=>e.ApplicationUserId == user!.Id));
+
+            var response = new StoreResponse
+            {
+                Id              = store!.Id,
+                StoreName       = store.StoreName,
+                Rating          = store.Rating,
+                RevenueShare    = store.RevenueShare,
+                CreatedAt       = store.CreatedAt,
+            };
+            // =========================================================
+            return Ok(response);
+            
+        }
+        #endregion
+    }
+}
---
+++ Areas/Store/DTOs/Response/StoreResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace TagerCom.Areas.Store.DTOs.Response
+{
+    public class StoreResponse
+    {
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? ApplicationUserId { get; set; } = string.Empty;
+        public string StoreName { get; set; } = null!;
+        public decimal Rating { get; set; } = 0m;
+        public int RevenueShare { get; set; } = 15; // Vendor's revenue share percentage (0.15 = 15%)
+        public bool IsActive { get; set; } = false;
+        [EnumDataType(typeof(StoreStatus))]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public StoreStatus Status { get; set; } = StoreStatus.Pending;
+        public bool IsDeleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
