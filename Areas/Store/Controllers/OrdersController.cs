ca7e291c446d7eead026f321c5a1345a878404d7
Yusif_Alaidy <110829739+Yusif-Alaidy@users.noreply.github.com>
2026-01-03 16:18:55 +0200

    feat: build get pending orders action

--- Areas/Store/Controllers/OrdersController.cs
+++ Areas/Store/Controllers/OrdersController.cs
@@ -375,6 +375,85 @@ namespace TagerCom.Areas.Store.Controllers
         }
         #endregion
 
+        #region Get Pending order
+        [HttpGet("pending")]
+        public async Task<IActionResult> GetPending([FromQuery] FilterOrdersRequest request)
+        {
+            // Get user ======================================================================
+            // ===============================================================================
+            var user = await UserManager.GetUserAsync(User);
+            // ===============================================================================
 
+            // Get store =====================================================================
+            // ===============================================================================
+            var store = await StoreRepo.GetOneAsync(e=>e.ApplicationUserId == user!.Id && !e.IsDeleted && e.IsActive);
+            if (store == null)
+            {
+                return NotFound(new { message = "This store is not exist" });
+            }
+            // ===============================================================================
+
+            // Get order =====================================================================
+            // ===============================================================================
+            var orders = await OrderRepo.GetAsync(e=>e.StoreId == store.Id && e.OrderStatus == OrderStatus.Pending, includes:[equals=>equals.Customer, ]);
+            // ===============================================================================
+
+            var totalPage = orders.Count() / 10;
+            // Filter ====================================================================================================
+            // ===========================================================================================================
+            if (request.startDate != null)
+            {
+                orders = orders.Where(e => e.CreatedAt >= request.startDate).ToList();
+            }
+            if (request.endDate != null)
+            {
+                orders = orders.Where(e => e.CreatedAt <= request.endDate).ToList();
+            }
+            if (request.orderStatus != null)
+            {
+                orders = orders.Where(e => e.OrderStatus == request.orderStatus).ToList();
+            }
+            if (!string.IsNullOrWhiteSpace(request.customerUsernameOrEmail))
+            {
+                orders = orders.Where(e => e.Customer.Email == request.customerUsernameOrEmail || e.Customer.UserName == request.customerUsernameOrEmail).ToList();
+            }
+            // ===========================================================================================================
+
+            // Sort ======================================================================================================
+            // ===========================================================================================================
+            if (request.newest)
+            {
+                orders = orders.OrderByDescending(e => e.CreatedAt).ToList();
+            }
+            else if (request.oldest)
+            {
+                orders = orders.OrderBy(e => e.CreatedAt).ToList();
+            }
+            // ===========================================================================================================
+            orders = orders.Skip(( request.page - 1 ) * request.pageSize).Take(request.pageSize).ToList();
+
+            // mapping =======================================================================
+            // ===============================================================================
+            var ordersDTO = orders.Select(e => new OrdersResponse
+            {
+                Id          = e.Id,
+                Customer    = e.Customer.UserName,
+                Store       = e.Store!.StoreName,
+                OrderStatus = e.OrderStatus,
+                TotalAmount = e.TotalAmount,
+                CreatedAt   = e.CreatedAt,
+            });
+            // ===============================================================================
+
+
+            return Ok(new
+            {
+                TotalPage = totalPage,
+                PageSize = request.pageSize,
+                Page = request.page,
+                ordersDTO,
+            });
+        }
+        #endregion
     }
 }
